8aeedd66e855137e8353debaa429fb30407f941c
yanhe <heroonehy@yahoo.com>
2018-04-19 19:55:17 +0800

    init]

---
+++ Memory/Memory/info.h
@@ -0,0 +1,13 @@
+//
+//  info.h
+//  Memory
+//
+//  Created by yanhe on 2018/4/19.
+//  Copyright © 2018年 yanhe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface info : NSObject
++(NSString *)getStringWithName:(int)nameId;
+@end
---
+++ Memory/Memory/info.m
@@ -0,0 +1,23 @@
+//
+//  info.m
+//  Memory
+//
+//  Created by yanhe on 2018/4/19.
+//  Copyright © 2018年 yanhe. All rights reserved.
+//
+
+#import "info.h"
+
+@implementation info
++(NSString *)getStringWithName:(int)nameId{
+    switch (nameId) {
+        case 0:
+            
+            break;
+            
+        default:
+            break;
+    }
+    return nil;
+}
+@end
